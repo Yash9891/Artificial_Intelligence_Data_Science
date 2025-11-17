c2b9f12662bfcd0178463962aa062f9a4da1eb08
Yash9891 <yash042002@gmail.com>
2025-11-17 18:59:00 +0530

    update

---
+++ Python_Fundamentals/05TypeConversion.py
@@ -0,0 +1,11 @@
+a=10
+b=5
+print(type(a/b))
+
+ans=3+6.0
+ans=int(ans)
+print(ans)
+print(type(ans))
+
+val=bool(10)
+print(val,type(val))
---
+++ Python_Fundamentals/06Input.py
@@ -0,0 +1,4 @@
+#By default input take sonly string data
+
+num=float(input("Enter a num"))
+print(f"{num:.2f}")
